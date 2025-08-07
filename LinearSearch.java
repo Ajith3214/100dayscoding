98f64e3458db160b42008f8e7b94cee04f7c7379
Ajith G <135093593+Ajith3214@users.noreply.github.com>
2025-08-07 19:42:11 +0530

    Add files via upload
    Day1 Linear and Binary search in arrays

---
+++ LinearSearch.java
@@ -0,0 +1,26 @@
+package com.sample;
+
+public class LinearSearch {
+	public static int linearSearch(int[] arr, int target) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == target) {
+                return i;  
+            }
+        }
+        return -1; 
+    }
+
+    public static void main(String[] args) {
+        int[] data = {5, 3, 8, 6, 7};
+        int target = 8;
+
+        int result = linearSearch(data, target);
+
+        if (result != -1) {
+            System.out.println("Element found at index " + result);
+        } else {
+            System.out.println("Element not found");
+        }
+    }
+
+}
