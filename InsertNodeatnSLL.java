5b4e27cdf15f98f2b3ab9c915efcc2da510fae1b
Ajith G <135093593+Ajith3214@users.noreply.github.com>
2025-08-13 22:24:14 +0530

    Add files via upload

--- InsertNodeatnSLL.java
+++ InsertNodeatnSLL.java
@@ -14,18 +14,18 @@ public class InsertNodeatnSLL {
 
     private Node head;
 
-    // Insert at n-th position (1-indexed)
+   
     public void insertAtPosition(int data, int position) {
         Node newNode = new Node(data);
 
-        // Case 1: Insert at head
+       
         if (position == 1) {
             newNode.next = head;
             head = newNode;
             return;
         }
 
-        // Traverse to the node before the desired position
+       
         Node current = head;
         for (int i = 1; i < position - 1; i++) {
             if (current == null || current.next == null) {
@@ -34,21 +34,12 @@ public class InsertNodeatnSLL {
             current = current.next;
         }
 
-        // Insert the node
+      
         newNode.next = current.next;
         current.next = newNode;
     }
 
-    // Print linked list
-//    public void printList() {
-//        Node temp = head;
-//        while (temp != null) {
-//            System.out.print(temp.data + " -> ");
-//            temp = temp.next;
-//        }
-//        System.out.println("NULL");
-//    }
-//    
+
     public void printList() {
         Node temp = head;
         while (temp != null) {
